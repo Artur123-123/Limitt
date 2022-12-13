e4b6b01e317cb504d576595f472f5ce0c7d28be0
Artur-123-123 <arturolejnik462@gmail.com>
2022-12-14 00:07:57 +0100

    Dodaj pliki projektów.

---
+++ limit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace limit
+{
+    internal class Program
+    {
+        class Elementy
+        {
+            public int[] nums = new int[10];
+
+            public void Limit()
+            {
+                int result;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    nums[i] = Convert.ToInt32(Console.ReadLine());
+                    if (nums[i] > 0 && nums[i] < 10)
+                    {
+                        result = nums[i];
+                    }
+                    else
+                    {
+                        i--;
+                        Console.WriteLine("Enter number between 1 and 9");
+                    }
+                }
+            }
+
+            public string ToText()
+            {
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Elementy element = new Elementy();
+            element.Limit();
+        }
+    }
+}
---
+++ limit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("limit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("limit")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("e1c50f15-60ba-45d9-a7ef-c77bf31be40b")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
